9946116bcfc97e311382c4caea0575c344559272
Tyler Hancock <darklime@live.ca>
2022-06-09 02:29:02 -0600

    Port to Minecraft 1.19

--- Fabric/src/main/java/net/darkhax/pigpen/PigPenFabric.java
+++ Fabric/src/main/java/net/darkhax/pigpen/PigPenFabric.java
@@ -1,13 +1,13 @@
 package net.darkhax.pigpen;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
 public class PigPenFabric implements ModInitializer {
 
     @Override
     public void onInitialize() {
 
-        CommandRegistrationCallback.EVENT.register(PigPenCommands::registerCommands);
+        CommandRegistrationCallback.EVENT.register((dispatcher, registry, environment) -> PigPenCommands.registerCommands(dispatcher, environment.includeDedicated));
     }
 }
